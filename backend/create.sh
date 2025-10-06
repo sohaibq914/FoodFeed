aff2037b9d08c3b5f5231630a8bb037aebcebeb0
Sohaib Qureshi <106452965+sohaibq914@users.noreply.github.com>
2025-10-06 12:05:53 -0400

    Add flask-socketio and eventlet installations

--- backend/create.sh
+++ backend/create.sh
@@ -4,3 +4,5 @@ python -m pip install flask
 python -m pip install supabase
 python -m pip install flask-cors
 python -m pip install python-dotenv
+python -m pip install flask-socketio
+python -m pip install eventlet
